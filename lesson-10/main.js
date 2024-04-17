8d430ffd05ab01bbc5a7582f39db9e6ea6350cd0
parkgun0517 <163221048+parkgun0517@users.noreply.github.com>
2024-04-17 01:48:39 +0000

    fin!

--- lesson-10/main.js
+++ lesson-10/main.js
@@ -2,3 +2,19 @@
 "use strict";
 
 // @TODO: 웹 서버를 만들고 실행한다.
+const port = 3000,
+    express = require('express'),
+    layout = require('express-ejs-layouts'),
+    homeController = require('./controllers/homeController'),
+    app = express();
+
+app.set("port", process.env.PORT || port);
+app.set("view engine", "ejs");
+
+app.use(layout);
+
+app.get('/name/:myName', homeController.respondWithName);
+
+app.listen(app.get("port"), () => {
+    console.log(`server at http://localhost:${app.get("port")}`);
+});
--- lesson-11/main.js
+++ lesson-11/main.js
@@ -2,3 +2,27 @@
 "use strict";
 
 // @TODO: 웹 서버를 만들고 실행한다.
+const port = 3000,
+    express = require('express'),
+    layout = require('express-ejs-layouts'),
+    homeController = require('./controllers/homeController'),
+    errorController = require('./controllers/errorController'),
+    app = express();
+
+app.set("port", process.env.PORT || port);
+app.set("view engine", "ejs");
+
+app.use(layout);
+app.use(express.static("public"));
+
+
+
+app.get('/name/:myName', homeController.respondWithName);
+
+app.use(errorController.logErrors);
+app.use(errorController.resNotFound);
+app.use(errorController.resInternalError);
+
+app.listen(app.get("port"), () => {
+    console.log(`server at http://localhost:${app.get("port")}`);
+});
